01f9bb7477f8988ec31211ec90bb84b02ffeed7c
oscar-quiroz <kairox.rt@gmail.com>
2020-07-19 11:31:11 -0500

    terinado --> falta el ciclo cada minuto

--- curl.sh
+++ curl.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 time_response=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.1.89:8765/)
-time_required=0,009321
+time_required=0,00899
 echo $time_required
 echo $time_response
 
+## realizar cada minuto
 if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_response" "$time_required"
 then
-     echo "el tiempo de respuesta es menor que el solicitado"
+     echo "################ Peticion realizada.#####################"
+    
+        
 else
-    echo el tiempo es mayor
+    echo " ERROR:: el tiempo de respuesta es mayor al tiempo definido "
+    ## conectarse por ssh 
+        sshpass -p '123456'  ssh server2@192.168.1.89  ./init.sh ##conectado
+        ##./init.sh ----> ejecuta bash que hace pull del ultimo commit y reiniciar server.js
+    
+        
+    ## se reinicia el serverdor remoto.
+    echo "#############servicio reiniciado.#####################"
 fi
