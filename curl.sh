caa1a79b6b83f8727d4fac869ee8e3d23c39a943
oscar-quiroz <kairox.rt@gmail.com>
2020-08-03 23:43:17 -0500

    bash que reinicia el proceso en el servidor

--- curl.sh
+++ curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-time_req=1,850
+time_req=1,350
 
 while true; do
     time_res=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.1.89:8765/)
@@ -9,21 +9,24 @@ while true; do
     echo 
 
     ##cada 5 segundos.
-    ## realizar cada minuto
-    if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_res" "$time_req"
-        then
-            echo "################ Peticion realizada.#####################"
-    else
-        echo "#### ERROR:: el tiempo de respuesta es mayor al tiempo que se ha definido. ####"
-        echo
-        ## conectarse por ssh 
-            sshpass -p '123456'  ssh -o StrictHostKeyChecking=no server2@192.168.1.89  'cd lab1;git pull; pm2 restart lab1' ##conectado
-            ##./init.sh ----> ejecuta bash que hace pull del ultimo commit y reiniciar server.js
-        echo
-        ## se reinicia el serverdor remoto.
-        echo "############# servicio reiniciado. #####################"
-        echo "########################################################"
-        echo
-    fi
+            ## realizar cada minuto
+            if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_res" "$time_req"
+                then
+                    echo "################ Peticion realizada.#####################"
+                    echo "########################################################"
+                    echo
+            else
+                echo "#### ERROR:: el tiempo de respuesta es mayor al tiempo que se ha definido. ####"
+                echo "############## Conectarse mediante SSh al servidor ############################"
+                ## conectarse por ssh 
+                    sshpass -p '123456'  ssh -o StrictHostKeyChecking=no server2@192.168.1.89  'cd lab1;git pull; pm2 restart 0' ##conectado
+                    ##./init.sh ----> ejecuta bash que hace pull del ultimo commit y reiniciar server.js
+                echo
+                ## se reinicia el serverdor remoto.
+                echo "############# servicio reiniciado. #####################"
+                echo "########################################################"
+                echo
+            fi
+
     sleep 5
 done
