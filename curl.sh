b0be3be779c855d2a81d06338441d84d9b9be9e4
oscar-quiroz <kairox.rt@gmail.com>
2020-08-03 23:05:35 -0500

    servidor y bash editados

--- curl.sh
+++ curl.sh
@@ -2,25 +2,28 @@
 time_req=1,850
 
 while true; do
-time_res=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.1.89:8765/)
-echo "tiempo requerido de respuesta: "  $time_req
-echo "tiempo de respuesta real:      "  $time_res
+    time_res=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.1.89:8765/)
+    echo "tiempo requerido de respuesta: "  $time_req
+    echo "tiempo de respuesta real:      "  $time_res
 
-echo 
+    echo 
 
-sleep 5 ##cada 5 segundos.
-## realizar cada minuto
-if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_res" "$time_req"
-then
-     echo "################ Peticion realizada.#####################"
-else
-    echo "#### ERROR:: el tiempo de respuesta es mayor al tiempo definido ####"
-    echo
-    ## conectarse por ssh 
-        sshpass -p '123456'  ssh -o StrictHostKeyChecking=no server2@192.168.1.89  'cd lab1;git pull; pm2 restart server.js' ##conectado
-        ##./init.sh ----> ejecuta bash que hace pull del ultimo commit y reiniciar server.js
-    echo
-    ## se reinicia el serverdor remoto.
-    echo "############# servicio reiniciado. #####################"
-fi
+    ##cada 5 segundos.
+    ## realizar cada minuto
+    if awk 'BEGIN{exit ARGV[1]>ARGV[2]}' "$time_res" "$time_req"
+        then
+            echo "################ Peticion realizada.#####################"
+    else
+        echo "#### ERROR:: el tiempo de respuesta es mayor al tiempo que se ha definido. ####"
+        echo
+        ## conectarse por ssh 
+            sshpass -p '123456'  ssh -o StrictHostKeyChecking=no server2@192.168.1.89  'cd lab1;git pull; pm2 restart lab1' ##conectado
+            ##./init.sh ----> ejecuta bash que hace pull del ultimo commit y reiniciar server.js
+        echo
+        ## se reinicia el serverdor remoto.
+        echo "############# servicio reiniciado. #####################"
+        echo "########################################################"
+        echo
+    fi
+    sleep 5
 done
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 const http = require('http');
-
+var port = 8765;
 
 const server = http.createServer(function (req, res) {
-    var num = getRandomArbitrary(1000, 2700);
-     console.log(num);
+    var num = generateRandomNumber(1000, 2700);
+    console.log(`tiempo minimo de respuesta: ${num}`);
     res.setTimeout( num , ()=>{
         res.writeHead(200, {'content-type': 'text/plain'});
         res.end('Hola Mundo');
@@ -12,8 +12,9 @@ const server = http.createServer(function (req, res) {
 
 });
 
-function getRandomArbitrary(min, max) {
+function generateRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
   }
 
-server.listen(8765);
+server.listen(port);
+console.log(`Servidor escuchando por el puerto ${port}`);
